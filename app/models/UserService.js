c874774d7e5692ff408f668309296be44078d8de
Alfredo Pereda <aperar1500@alumno.ipn.mx>
2022-05-03 00:52:38 -0500

    Implementation test and static method of getInfo

--- app/models/UserService.js
+++ app/models/UserService.js
@@ -1,10 +1,14 @@
 const user = require("./../models/user")
 
 class userService{
-    
+
     static create(id,username, name){
-        return new User(id, username, name, "Sin Bio")
+        return new user(id, username, name, "Sin Bio")
+    }
+
+    static getInfo(userS){
+        return [userS.id, userS.username,userS.name, "Sin Bio"]
     }
 }
 
-module.exports = userService()
+module.exports = userService
--- test/models/userService.test.js
+++ test/models/userService.test.js
@@ -1,16 +1,28 @@
-const user = require('./../../app/models/user')
+
 const UserService = require('./../../app/models/UserService')
 
 describe("Unit Tests for UserService class", () =>{
     test("Create a new user using the UserService", () =>{
         //lo que voy hacer
-        const user  = new UserService(3,"alfello","Alfredo", "I'm learning")
+        const user  = UserService.create(3,"alfello","Alfredo", "I'm learning")
 
         //validación
 
+        expect(user.id).toBe(3)
         expect(user.username).toBe("alfello")
-        expect(user.id).toBe("3")
         expect(user.name).toBe("Alfredo")
         expect(user.description).not.toBeUndefined()
     })
+})
+
+describe("Unit Test for the method getInfo in UserService class",() => {
+    test("Test the static method getInfo", () => {
+        const userS = UserService.create(3,"alfello","Alfredo", "I'm coding") 
+        const userInfoList = UserService.getInfo(userS)
+        
+        expect(userInfoList[0]).toBe(3)
+        expect(userInfoList[1]).toBe("alfello")
+        expect(userInfoList[2]).toBe("Alfredo")
+        expect(userInfoList[3]).toBe("Sin Bio")
+    });
 })
